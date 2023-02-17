facb75f8b89e5815d1dd2b69fed432ea28f6f7a6
Gooonzalo <g.ortega.2019@alumnos.urjc.es>
2023-02-17 19:13:59 +0100

    Project initilization complete. - CSS loading problem fixed addind dependencies. - Pages relinked with WebController.java

--- readme-bookstore/src/main/java/es/codeurjc/readmebookstore/WebController.java
+++ readme-bookstore/src/main/java/es/codeurjc/readmebookstore/WebController.java
@@ -7,24 +7,74 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/greeting")
-	public String greeting(Model model) {
-
-		model.addAttribute("name", "Mundo");
-
-		return "greeting_template";
-	}
-
 	@GetMapping("/")
 	public String home(Model model) {
-
 		return "index";
 	}
 
-	@GetMapping("/books-general-page.html")
-	public String books(Model model) {
+	@GetMapping("/index.html")
+	public String index(Model model) {
+		return "index";
+	}
 
+	@GetMapping("/admin-page.html")
+	public String admin(Model model) {
+		return "admin-page";
+	}
+
+	@GetMapping("/book-particular-page.html")
+	public String bookParticular(Model model) {
+		return "book-particular-page";
+	}
+
+	@GetMapping("/books-general-page.html")
+	public String booksGeneral(Model model) {
 		return "books-general-page";
 	}
-    
+
+	@GetMapping("/checkout-page.html")
+	public String checkout(Model model) {
+		return "checkout-page";
+	}
+
+	@GetMapping("/contact-page.html")
+	public String contact(Model model) {
+		return "contact-page";
+	}
+
+	@GetMapping("/login-page.html")
+	public String login(Model model) {
+		return "login-page";
+	}
+
+	@GetMapping("/offer-page.html")
+	public String offer(Model model) {
+		return "offer-page";
+	}
+
+	@GetMapping("/register-page.html")
+	public String register(Model model) {
+		return "register-page";
+	}
+
+	@GetMapping("/statistics-page.html")
+	public String statistics(Model model) {
+		return "statistics-page";
+	}
+
+	@GetMapping("/upload-offer-page.html")
+	public String uploadOffer(Model model) {
+		return "upload-offer-page";
+	}
+
+	@GetMapping("/upload-review-page.html")
+	public String uploadReview(Model model) {
+		return "upload-review-page";
+	}
+
+	@GetMapping("/user-page.html")
+	public String user(Model model) {
+		return "user-page";
+	}
+
 } 
