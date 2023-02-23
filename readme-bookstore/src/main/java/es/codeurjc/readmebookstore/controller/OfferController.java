3c993095deb14e63eea3fb2e4cca244eaffae7ad
sergioHernandez2 <sergio2hs01@gmail.com>
2023-02-23 23:24:44 +0100

    Added the funcionality of upload offers in a book. - Created the offer controler. - Introduced a text if there are no reviews and offers. - Introduced the name of the user in the review. - Introduced the name of the book in the offer.

--- readme-bookstore/src/main/java/es/codeurjc/readmebookstore/controller/OfferController.java
+++ readme-bookstore/src/main/java/es/codeurjc/readmebookstore/controller/OfferController.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import jakarta.servlet.http.HttpServletRequest;
 import es.codeurjc.readmebookstore.model.User;
 import es.codeurjc.readmebookstore.model.Book;
-import es.codeurjc.readmebookstore.model.Review;
+import es.codeurjc.readmebookstore.model.Offer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import es.codeurjc.readmebookstore.repository.ReviewRepository;
 import es.codeurjc.readmebookstore.repository.UserRepository;
 import es.codeurjc.readmebookstore.repository.BookRepository;
+import es.codeurjc.readmebookstore.repository.OfferRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-public class ReviewController {
+public class OfferController {
 
     @Autowired
     private UserRepository userRepository;
@@ -28,27 +28,27 @@ public class ReviewController {
     private BookRepository bookRepository;
 
     @Autowired
-    private ReviewRepository reviewRepository;
+    private OfferRepository offerRepository;
 
 
  
-	@PostMapping("/uploadedReview/{id}")
-	public String uploadedReview(Model model, @PathVariable long id, @RequestParam String text, HttpServletRequest request) {
+	@PostMapping("/uploadedOffer/{id}")
+	public String uploadedReview(Model model, @PathVariable long id, @RequestParam String edition, @RequestParam String text, @RequestParam float price, HttpServletRequest request) {
         Optional<Book> book = bookRepository.findById(id);
         String username = request.getUserPrincipal().getName();
 		User user = userRepository.findByName(username).orElseThrow();
         Date date = new Date();
-        Review review = new Review(text, date, book.get(), user);
-		reviewRepository.save(review);
+        Offer offer = new Offer(date, edition, text, price, book.get(), user);
+		offerRepository.save(offer);
 		return "redirect:/book/"+ book.get().getId();
 	}
 
 
-    @GetMapping("/uploadReview/{id}")
-	public String uploadReview(Model model, @PathVariable long id) {
+    @GetMapping("/uploadOffer/{id}")
+	public String uploadOffer(Model model, @PathVariable long id) {
         Optional<Book> book = bookRepository.findById(id);
         model.addAttribute("book", book.get());
-		return "upload-review-page";
+		return "upload-offer-page";
 	}
 
   
