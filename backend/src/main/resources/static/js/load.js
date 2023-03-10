eeae7ca68632e579528c3bb92debf049905ba690
mmartinal <49288214+mmartinal@users.noreply.github.com>
2023-03-10 17:15:27 +0100

    Load more search functional

--- backend/src/main/resources/static/js/load.js
+++ backend/src/main/resources/static/js/load.js
@@ -1,26 +1,50 @@
 var currentPage = 0;
+const valores = window.location.search;
 
-function moreContent(searchtext) {
+function moreContent() {
     switchMoreContentButtonActivation(true)
     currentPage = currentPage + 1;
-    $.ajax({
-        type: 'GET',
-        url: '/books?searchtext=' + searchtext + '&currentPage=' + currentPage,
-        error: function () {
-            console.log('Something went wrong')
-            switchMoreContentButtonActivation(false)
-        },
-        success: function (data) {
-            var books = $(data).find('#Books').html();
-            var noMoreBooks = $(data).find('#noMoreBooks').html();
-            if (noMoreBooks == undefined) {
-                $('#Books').append(books)
+    console.log(valores);
+    const urlParams = new URLSearchParams(valores);
+    var searchtext = urlParams.get('searchtext');
+    if (searchtext == null)
+        $.ajax({
+            type: 'GET',
+            url: '/books?currentPage=' + currentPage,
+            error: function () {
+                console.log('Something went wrong')
                 switchMoreContentButtonActivation(false)
-            } else {
-                $('#moreContentButton').css("display", "none");
+            },
+            success: function (data) {
+                var books = $(data).find('#Books').html();
+                var noMoreBooks = $(data).find('#noMoreBooks').html();
+                if (noMoreBooks == undefined) {
+                    $('#Books').append(books)
+                    switchMoreContentButtonActivation(false)
+                } else {
+                    $('#moreContentButton').css("display", "none");
+                }
             }
-        }
-    })
+        }) 
+    else
+        $.ajax({
+            type: 'GET',
+            url: '/books?searchtext=' + searchtext + '&currentPage=' + currentPage,
+            error: function () {
+                console.log('Something went wrong')
+                switchMoreContentButtonActivation(false)
+            },
+            success: function (data) {
+                var books = $(data).find('#Books').html();
+                var noMoreBooks = $(data).find('#noMoreBooks').html();
+                if (noMoreBooks == undefined) {
+                    $('#Books').append(books)
+                    switchMoreContentButtonActivation(false)
+                } else {
+                    $('#moreContentButton').css("display", "none");
+                }
+            }
+        })
 }
 
 var currentReviewsPage = 0
