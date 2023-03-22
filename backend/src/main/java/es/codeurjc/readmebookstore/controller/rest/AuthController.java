63f6dbfc638b383d176008a2a4e06607aa3f3ae5
amarkosmarkos <64153022+amarkosmarkos@users.noreply.github.com>
2023-03-22 17:00:47 +0100

    Basic login, logout and security with API (not completely working)

---
+++ backend/src/main/java/es/codeurjc/readmebookstore/controller/rest/AuthController.java
@@ -0,0 +1,48 @@
+package es.codeurjc.readmebookstore.controller.rest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.codeurjc.readmebookstore.security.jwt.AuthResponse;
+import es.codeurjc.readmebookstore.security.jwt.LoginRequest;
+import es.codeurjc.readmebookstore.security.jwt.UserLoginService;
+import es.codeurjc.readmebookstore.security.jwt.AuthResponse.Status;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+	@Autowired
+	private UserLoginService userService;
+
+	@PostMapping("/login")
+	public ResponseEntity<AuthResponse> login(
+			@CookieValue(name = "accessToken", required = false) String accessToken,
+			@CookieValue(name = "refreshToken", required = false) String refreshToken,
+			@RequestBody LoginRequest loginRequest) {
+		
+		return userService.login(loginRequest, accessToken, refreshToken);
+	}
+
+	@PostMapping("/refresh")
+	public ResponseEntity<AuthResponse> refreshToken(
+			@CookieValue(name = "refreshToken", required = false) String refreshToken) {
+
+		return userService.refresh(refreshToken);
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+
+		return ResponseEntity.ok(new AuthResponse(Status.SUCCESS, userService.logout(request, response)));
+	}
+}
