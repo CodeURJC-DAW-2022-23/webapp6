66e5b5554429797cb06f8181284be2cb57f241be
mmartinal <49288214+mmartinal@users.noreply.github.com>
2023-03-29 00:23:28 +0200

    script to create app docker image

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,9 +1,4 @@
-
+# Script to create application image.
 
 docker build -t readmebookstore/webapp6 -f docker/Dockerfile .
-docker push 
-
-
-# -d runs the container in background
-# -p binds the containter and the local host ports
-docker run -d -p 8443:8443 readme_image
+docker push readmebookstore/webapp6
