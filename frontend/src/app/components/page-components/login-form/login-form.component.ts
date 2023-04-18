91512648d899e8d697bdf4ad21dd7b16c0328007
amarkosmarkos <64153022+amarkosmarkos@users.noreply.github.com>
2023-04-18 20:07:57 +0200

    Logout Done and design improved

--- frontend/src/app/components/page-components/login-form/login-form.component.ts
+++ frontend/src/app/components/page-components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'login-form',
@@ -9,9 +10,15 @@ export class LoginFormComponent {
   username: string = '';
   password: string = '';
 
+  constructor(public loginService: LoginService) { }
+
   @Output() onLogIn = new EventEmitter<{username: string, password: string}>();
 
   submitLoginForm() {
     this.onLogIn.emit({username: this.username, password: this.password});
+  }
+
+  logOut() {
+    this.loginService.logOut();
   }
 }
