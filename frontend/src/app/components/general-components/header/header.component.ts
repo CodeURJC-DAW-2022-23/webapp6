293056fe2acfffd28424b47aea1efe74932d6c00
mmartinal <49288214+mmartinal@users.noreply.github.com>
2023-04-19 19:55:50 +0200

    Header

--- frontend/src/app/components/general-components/header/header.component.ts
+++ frontend/src/app/components/general-components/header/header.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit, Input, Output, EventEmitter } from '@angular/core';
+//import { CartService } from 'src/app/services/cart.service';
+import { User } from 'src/app/models/user.model';
+import { LoginService } from 'src/app/services/login.service';
+//import { PostsService } from 'src/app/services/posts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'header-main',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  @Output()
+  show = new EventEmitter();
+
+  @Output()
+  filterTags = new EventEmitter<{id: number, search: string}>();
+
+
+  //Info usuario
+  user: User | undefined;
+  isLogged: boolean | undefined ;
+  isAdmin: boolean | undefined;
+
+
+  constructor(
+    private router: Router,
+    private LoginService: LoginService) {
+  }
+
+  ngOnInit() {
+    this.isLogged = this.LoginService.isLogged();
+    if (this.isLogged) {
+      this.user = this.LoginService.currentUser();
+      if (this.user != undefined)
+        this.isAdmin = this.user.roles.includes("ROLE_ADMIN");
+    }
+  }
+
+  logOut() {
+    this.LoginService.logOut();
+    this.router.navigate(['']).then(_ =>
+      window.location.reload()
+    );
+  }
+
 
 }
--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -3,4 +3,5 @@ export interface User {
     name: string;
     email: string;
     image: boolean;
+    roles: Array<string>;
 }
