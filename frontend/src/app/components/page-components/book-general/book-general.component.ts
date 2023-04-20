199b645ade5dce5343660d313eca03f9bdba6a49
Alberto Jiménez Gómez <49287820+kaydas@users.noreply.github.com>
2023-04-20 13:59:08 +0200

    Books general page
    Upload almost finished book general page

---
+++ frontend/src/app/components/page-components/book-general/book-general.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Page } from 'src/app/models/page.model';
+import { BookService } from 'src/app/services/book.service';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'book-general',
+  templateUrl: './book-general.component.html',
+  styleUrls: ['./book-general.component.css']
+})
+export class BookGeneralComponent {
+
+  books: Page | undefined;
+  booksPage = 0;
+  booksButton = true;
+
+  constructor(private router: Router, activatedRoute: ActivatedRoute, public bookService: BookService, public loginService: LoginService) {
+
+    this.bookService.getBooksPaginated(0).subscribe(
+        books => this.books = books,
+        error => console.log(error)
+      );
+    }
+
+    loadMoreBooks(n: number) {
+        this.booksPage = n;
+        this.bookService.getBooksPaginated(n).subscribe(
+        newBooks => {
+            if (this.books != undefined && newBooks.numberOfElements != 0) {
+            this.books.content = this.books.content.concat(newBooks.content);
+            } else {
+            this.booksButton = false;
+            }
+        },
+        error => console.log(error)
+        );
+    }
+}
