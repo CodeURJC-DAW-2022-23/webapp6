10e1b056637157e2af5cdc2a031872908026e65e
mmartinal <49288214+mmartinal@users.noreply.github.com>
2023-04-23 00:03:37 +0200

    bugs fixed

--- frontend/src/app/components/general-components/header/header.component.ts
+++ frontend/src/app/components/general-components/header/header.component.ts
@@ -12,39 +12,16 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output()
-  show = new EventEmitter();
-
-  @Output()
-  filterTags = new EventEmitter<{id: number, search: string}>();
-
-
-  //Info usuario
-  user: User | undefined;
-  isLogged: boolean | undefined ;
-  isAdmin: boolean | undefined;
-
-
   constructor(
     private router: Router,
     public LoginService: LoginService) {
   }
 
   ngOnInit() {
-    this.isLogged = this.LoginService.isLogged();
-    if (this.isLogged) {
-      this.user = this.LoginService.currentUser();
-      if (this.user != undefined)
-        this.isAdmin = this.user.roles.includes("ROLE_ADMIN");
-    }
+
   }
 
-  logOut() {
-    this.LoginService.logOut();
-    this.router.navigate(['']).then(_ =>
-      window.location.reload()
-    );
-  }
+
 
 
 }
