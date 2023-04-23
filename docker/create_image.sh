38800ee7d3d13aaae91337a42dfca407f060a415
Gooonzalo <66415975+Gooonzalo@users.noreply.github.com>
2023-04-23 19:03:24 +0200

    Updated docker script to generate Angular production files.

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,8 +1,14 @@
-# Script to create application image and upload it to Doker Hub.
 
+# Generate Angular production files, from frontend directory:
+ng build --configuration production --base-href="/new/"
+
+# Make the new folder in the static directory in the backend and move generated files to it:
+mkdir ../backend/src/main/resources/static/new
+cp dist/frontend/* ../backend/src/main/resources/static/new
+
+# Create application image and upload it to Doker Hub.
 docker build -t readmebookstore/webapp6 -f docker/Dockerfile .
 docker push readmebookstore/webapp6
 
-# Script run the doker compose with images from our app and a SQL database.
-
+# Run the doker compose with images from our app and a SQL database.
 docker-compose -f docker/docker_compose.yml up
